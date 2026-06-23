875c084d1d3a07b0a3ee0369bdb269922bac2c0e
User <user@example.com>
2026-06-23 11:36:21 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
 export default defineConfig({
   site: 'https://example.com',
   outDir: 'docs',
+  integrations: [vue()],
 });
